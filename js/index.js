ad8a05517683f04b6cf8ec2135b439862e4a9357
Anowervaiya <mdanowerhossen727@gmail.com>
2024-03-02 16:06:42 +0600

    project completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const allButtonContainer = document.getElementById('allButtonContainer');
 
-const cardContainer = document.getElementById('cardContainer');
+
+const cardContainerId = document.getElementById('cardContainer');
+
+const selectedId = '08';
 
 const showButton = async () => {
   const responsive = await fetch(
@@ -8,46 +11,66 @@ const showButton = async () => {
   );
   const data = await responsive.json();
   data.data.news_category.forEach((item) => {
-    // console.log(item);
+
+   console.log(item)
     const btn = document.createElement('button');
     btn.classList.add('text-gray-400');
     btn.innerText = item.category_name;
     allButtonContainer.appendChild(btn);
+    btn.addEventListener('click', () => {
+      cardShow(item.category_id)
+      // console.log('clicked')
+    });
+    
   })
+ 
 };
 
-const cardShow = async() => {
+const inputId = () => {
+  const inputId = document.getElementById('inputId').value;
+  console.log(inputId)
+  cardShow(inputId);
+
+}
+
+
+const cardShow = async (id) => {
+  // console.log(id)
+ 
   const responsive = await fetch(
-    'https://openapi.programming-hero.com/api/news/category/01'
+    `https://openapi.programming-hero.com/api/news/category/${id}`
   );
   const data = await responsive.json();
+  cardContainerId.innerHTML = '';
   data.data.forEach((item) => {
+    // console.log(item);
+    
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `
     
       <div class="card card-side p-4 bg-base-100 shadow-xl">
-        <figure><img class="rounded-lg" src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
+        <img class="rounded-lg w-[280px] lg:w-[420px]" src="${item.image_url}" alt="Movie" />
+        <div class=" px-8 py-2">
+          <h2 class="card-title">${item.title}</h2>
+          <p class="py-3 text-[#949494]">${item.details.slice(0,500)}</p>
           <div class="flex justify-between items-center">
             <!-- profile -->
             <div class="flex justify-center items-center gap-4">
               <div>
-                <img class="w-[50px] h-[50px] rounded-full" src="images/carbon_view.png" alt="">
+                <img class=" w-[50px] h-[50px] rounded-full" src="${item.author?.img}" alt="">
               </div>
               <div>
-                <p class="text-xl font-bold">Jane coper</p>
-                <p class="text-[#718797]">jan 10,2022</p>
+                <p class="text-xl font-bold">${item.author.name}</p>
+                <p class="text-[#718797]">${item.author?.published_date}</p>
               </div>
             </div>
             <!-- eye contact  -->
-            <div>
+            <div class="flex gap-2 items-center">
               <div>
                 <img src="images/carbon_view.png" alt="">
               </div>
               <div>
-                <span>1.5M</span>
+                <span>${item.total_view}</span>
               </div>
             </div>
             <!-- start conatact  -->
@@ -62,10 +85,10 @@ const cardShow = async() => {
         </div>
       </div>
     `;
-    cardContainer.appendChild(newDiv);
+    cardContainerId.appendChild(newDiv);
 
   })
 }
-cardShow();
+cardShow(selectedId);
 
 showButton();
